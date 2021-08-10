2c5fa7f533e156847f58a10fd5ac1ef3153476e2
Nick Cave <nxdmedia@googlemail.com>
2021-08-10 20:14:30 +0100

    Create both User and Recipe models for the database, also assigned starting datatypes

---
+++ api/models/recipe.js
@@ -0,0 +1,109 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Recipe.init({
+    recipeId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING, 
+      allowNull: false, 
+      validate: {
+        notNull: {
+          msg: "Please provide a title"
+        },
+        notEmpty: {
+          msg: "Please provide a valid title"
+        }
+      }
+    }, 
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a description"
+        },
+        notEmpty: {
+          msg: "Please provide a valid description"
+        }
+      }
+    }, 
+    estimatedTime: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      validate: {
+        notNull: {
+          msg: "Please provide an estimated time in minutes (e.g 20mins)"
+        },
+        notEmpty: {
+          msg: "Please provide a valid time in minutes (e.g 20mins)"
+        }
+      }
+    }, 
+    category: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please specify which category this recipe falls under"
+        },
+        notEmpty: {
+          msg: "A category is needed"
+        }
+      }
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide the cooking instructions"
+        },
+        notEmpty: {
+          msg: "Please provide a valid set of cooking instructions"
+        }
+      }
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide the ingredients"
+        },
+        notEmpty: {
+          msg: "Please provide a valid set of ingredients"
+        }
+      }
+    },
+    displayImage: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please add a image link for the recipe"
+        },
+        notEmpty: {
+          msg: "Please add a valid image link for the recipe"
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Recipe',
+  });
+  return Recipe;
+};
---
+++ api/models/user.js
@@ -0,0 +1,80 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    userId: {
+      type: DataTypes.INTERGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A first name is required"
+        }, 
+        notEmpty: {
+          msg: "Please provide a first name"
+        }
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A last name is required"
+        }, 
+        notEmpty: {
+          msg: "Please provide a last name"
+        }
+      }
+    },
+    emailAddress: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+      unique: {
+        msg: "That email already exists."
+      },
+      validate: {
+        notNull: {
+          msg: "An email address is required"
+        }, 
+        isEmail: {
+          msg: "Please provide a valid email"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A password is required"
+        },
+        notEmpty: {
+          msg: "Please provide a password"
+        }
+      },
+      // set(val) {
+      //   const hashedPassword = bcrypt.hashSync(val, 10);
+      //   this.setDataValue('password', hashedPassword);
+      // }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
