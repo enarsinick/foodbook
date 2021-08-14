b8c7f4172d2dd6eeae1f9df60e5cc4fd398b8e7f
Nick Cave <nxdmedia@googlemail.com>
2021-08-14 13:42:00 +0100

    Added relationship between each database schema

--- api/models/recipe.js
+++ api/models/recipe.js
@@ -8,7 +8,21 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Recipe.belongsTo(models.User, {
+        as: "owner",
+        foreignKey: {
+          fieldName: "userId",
+          allowNull: false,
+          validate: {
+            notNull: {
+              msg: "A course owner ID is required",
+            },
+            notEmpty: {
+              msg: "Please provide a course owner ID"
+            }
+          }
+        }
+      })
     }
   };
   Recipe.init({
